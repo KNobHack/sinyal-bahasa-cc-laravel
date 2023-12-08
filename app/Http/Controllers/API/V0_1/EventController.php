0e4509fbf2ec88c8e5d24dd7595fb07a0449bc7d
KNobHack <akudandia7272@gmail.com>
2023-12-09 00:11:03 +0700

    DefaultAcceptForAPI Middleware

---
+++ app/Http/Controllers/API/V0_1/EventController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers\API\V0_1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\V0_1\EventResource;
+use App\Models\Event;
+use Illuminate\Http\Request;
+
+class EventController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        if ($request->get('paginate') === '1') {
+            $event = Event::paginate();
+        } else {
+            $event = Event::all();
+        }
+
+        return EventResource::collection($event);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Event $event)
+    {
+        return new EventResource($event);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Event $event)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Event $event)
+    {
+        //
+    }
+}
