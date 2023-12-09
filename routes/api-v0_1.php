8446de2567fe0c8cf98a70cf1c16e60cdbc91d43
KNobHack <akudandia7272@gmail.com>
2023-12-09 21:10:49 +0700

    Fix json format in doc AuthWithTokenResponse

--- routes/api-v0_1.php
+++ routes/api-v0_1.php
@@ -34,9 +34,9 @@ use Illuminate\Support\Facades\Route;
  * @apiSuccessExample 200
  *     HTTP/1.1 200 OK
  *     {
- *         "access_token" => "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c",
- *         "token_type" => "bearer",
- *         "expires_in" => 3600,
+ *         "access_token" : "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c",
+ *         "token_type" : "bearer",
+ *         "expires_in" : 3600,
  *         "user" => {
  *             "id": 2
  *             "name": "Fany Muhammad Fahmi Kamilah",
